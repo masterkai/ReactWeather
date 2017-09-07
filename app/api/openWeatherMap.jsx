f969f5e964903719ec6e7b1b51e9407a01bca853
Unknown <kai0919@hotmail.com>
2017-09-07 17:10:16 +0800

    ErrorModal completed

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,20 +1,20 @@
 var axios = require('axios');
 
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=d05a5a366fdba884be298635dee75c0f&units=metric';
-// d05a5a366fdba884be298635dee75c0f
-module.exports = {
-    getTemp: function (location) {
-        var encodedLocation = encodeURIComponent(location);
-        var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-        return axios.get(requestUrl).then(function (res) {
-            if (res.data.cod && res.data.message) {
-                throw new Error(res.data.message);
-            }else {
-                return res.data.main.temp;
-            }
-        }, function (err) {
-            throw new Error(err.response.data.message);
-        });
-    }
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
 }
